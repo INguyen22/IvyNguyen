9fc5f3b301adeab4368fc46534ef417780c92cc4
Ivy Nguyen <animeweirdo24@gmail.com>
2022-10-28 15:20:07 -0600

    style icons and add navbar

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,19 +3,21 @@ import ivy from "./20221018_180042.png"
 import resumeIcon from "./resume-logo.png"
 import pdf from "./Nguyen_resume.pdf"
 import { SocialIcon } from 'react-social-icons';
+import NavBar from './NavBar';
 import "./Homepage.css"
 
 const Homepage = () => {
   return (
     <div className='homepage'>
+      <NavBar />
         <img className="portfolioImage" src={ivy} alt="ivy portfolio image"/>
         <h1>Ivy Nguyen</h1>
         <div className='iconContainer'>
-            <SocialIcon url="https://www.linkedin.com/in/ivy-nguyen-051b27212/" />
-            <SocialIcon url="https://github.com/INguyen22" />
-            <a href={pdf}><img className="resume" src={resumeIcon} alt="ivy resume icon"/></a>
+            <SocialIcon className="icons" url="https://www.linkedin.com/in/ivy-nguyen-051b27212/" />
+            <SocialIcon className="icons" url="https://github.com/INguyen22" />
+            <a href={pdf}><button className="icons" id='resume'>Resume</button></a>
         </div>
-        <p>Hi! My name is Ivy and I am a Software Developer with a focus on Front-End tech</p>
+        <p className='intro'>Hi! My name is Ivy and I am a Software Developer with a focus on Front-End tech</p>
     </div>
   )
 }
